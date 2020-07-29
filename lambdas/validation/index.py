c2a168ed6e42e3c397e93f3175cb81c0a52d77c0
Rodrigues <artrodri@a483e71ad5e7.ant.amazon.com>
2020-07-29 10:29:06 -0700

    validation lambda ready

--- lambdas/validation/index.py
+++ lambdas/validation/index.py
@@ -11,44 +11,33 @@ import ntpath
 
 queueUrl = os.getenv('queueUrl')
 
-s3 = boto3.resource('s3')
-
-sqs = boto3.resource('sqs')
-queue = sqs.Queue(queueUrl)
+s3_client = boto3.client('s3')
+sqs = boto3.client('sqs')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def saveErrorStatusMsg(s3bucket, key, msg):
-    txtMsg = '{ "code": 3, "msg": ' + msg +  '}'
-    s3.Bucket(s3bucket).upload_file(txtMsg,key)
+def setStatusMsg(s3bucket, key, msg, code):
+    txtMsg = '{ "code": ' + str(code) + ', "msg": "' + msg +  '"}'
+    with open("/tmp/status.json", "w") as text_file:
+        text_file.write(txtMsg)
+    s3_client.upload_file('/tmp/status.json',s3bucket,key)
 
-def deleteMessage():
-    response = queue.receive_messages(
-        AttributeNames=['All'],
-        MaxNumberOfMessages=10,
-        VisibilityTimeout=10,
-        WaitTimeSeconds=10
+def sendSqsMessage(body):
+    logger.info("Adding msg to the queue: " + queueUrl)
+    response = sqs.send_message(
+        QueueUrl=queueUrl,
+        MessageBody=json.dumps(body)
     )
-
-    for message in response:
-        sqsEvent = message.body
-        sqsFile = urllib.parse.unquote(sqsEvent["Records"][0]["s3"]["object"]["key"])
-        sqsbucket = urllib.parse.unquote(sqsEvent["Records"][0]["s3"]["bucket"]["name"])
-
-        if sqsFile == zipFileKey:
-            logger.info("found it")
-            rsp = message.delete
-            logger.info(rsp)
 
 def ziptest(filename):
     file_count = 0
     try:
         zipfile.is_zipfile(filename)        
     except:
-        logger.error("This file is not a Zip file")
+        logger.info("This file is not a Zip file")
         return "Error: This file is not a Zip file"
-    zipfile.ZipFile(filename).extractall("/tmp/testfolder")
+    zipfile.ZipFile(filename).extractall(path="/tmp/testfolder")
     logger.info("Extracting zip files")
     files = []
     for r, d, f in os.walk("/tmp/testfolder"):
@@ -61,7 +50,7 @@ def ziptest(filename):
             ds = dcmread(f)
             file_count += 1
         except:
-            logger.error("This file is not a DCM file")
+            logger.info("This file is not a DCM file: " + f)
             return "Error: Zip does not contain DCM files"
 
     logger.info("Validated " + str(file_count) + " dcm files")
@@ -71,28 +60,25 @@ def ziptest(filename):
 def handler(event, context):
 
     #logger.info(event)
-    statusFile = urllib.parse.unquote(event["Records"][0]["s3"]["object"]["key"])
+    zipKey = urllib.parse.unquote(event["Records"][0]["s3"]["object"]["key"])
     bucket = urllib.parse.unquote(event["Records"][0]["s3"]["bucket"]["name"])
 
-    obj = s3.Object(bucket, statusFile)
-    status = json.loads(obj.get()['Body'].read())
-    if status["code"] != 0:
-        logger.info("code is not 0 - skipping")
+    print(zipKey)
 
-    zipFileKey = statusFile[0:len(statusFile)-7]
-    zipfileName = ntpath.basename(zipFileKey)
-
-    s3.Bucket(bucket).download_file(zipFileKey, '/tmp/' + zipfileName)
-
-    rst = ziptest('/tmp/' + zipfileName)
-
-    if (rst[:5].lower()) == "error":
-        saveErrorStatusMsg(bucket, statusFile, rst)
-        deleteMessage()
+    if len(zipKey) < 2:
+        logger.error("something went wrong")
         return False
 
-    
+    zipFilePath = '/tmp/' + ntpath.basename(zipKey)
 
+    s3_client.download_file(bucket, zipKey, zipFilePath)
 
-        
+    rst = ziptest(zipFilePath)
 
+    if (rst[:5].lower()) == "error":
+        setStatusMsg(bucket, zipKey + '.status', rst, 3)
+        return False
+    else:
+        sendSqsMessage(event)
+        setStatusMsg(bucket, zipKey + '.status', 'Sent to the queue', 0)
+        return True
